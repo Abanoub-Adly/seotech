13877a7fcbce24a2da16b2dcc928066ff6e37507
Abanoub <abanoubadly832@gmail.com>
2023-06-22 23:13:14 +0300

    new update

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let navbar = document.querySelector(".navbar");
 let menuLis = Array.from(document.querySelectorAll(".navbar ul li"));
 let bulletsButton = document.querySelector(".bullets");
 let bulletsSpans = document.querySelectorAll(".bullets span");
-let homePage = document.getElementById("home");
+let homePage = document.getElementById(".home");
 let navButtons = document.querySelector(".navbuttons");
 let rightButton = document.querySelector(".right");
 let leftButton = document.querySelector(".left");
-let testimonialBox = document.getElementById("test-box");
+let testimonialBox = document.getElementById("t.est-box");
 let testimonial = document.querySelector(".testimonial");
 let dateNow = document.querySelector(".datenow");
-let footerUlis = document.querySelectorAll(".footer-ul li");
+let footerUlis = document.querySelectorAll(".footer-ul a");
 menuLis.forEach((li)=>{
     li.addEventListener("click",(e)=>{
     removeActive();
@@ -34,8 +34,8 @@ bulletsButton.addEventListener("click",()=>{
             bulletsButton.classList.toggle("closeMenu");
         }
         else{
-            homePage.style.cssText = "height:calc(170vh - 20px)";
-            navButtons.style.cssText = "bottom: -35%";
+            homePage.style.cssText = "height:calc(200vh - 20px)";
+            navButtons.style.cssText = "bottom: -61%";
             menu.style.display = "flex";
             bulletsSpans.forEach((span)=>{
             span.classList.add("close");
@@ -53,7 +53,7 @@ bulletsButton.addEventListener("click",()=>{
         navbar.classList.toggle("navbarclose");
     }
     else{
-        navbar.style.cssText = "height:calc(40vh - 20px)";
+        navbar.style.cssText = "height:calc(50vh - 20px)";
         menu.style.display = "flex";
         bulletsSpans.forEach((span)=>{
         span.classList.add("close");
@@ -62,18 +62,24 @@ bulletsButton.addEventListener("click",()=>{
         navbar.classList.toggle("navbarclose");
         }
     })
-window.onscroll=()=>{
-    if(window.scrollY >= testimonial.offsetTop - 600){
-        testimonialBox.style.right = "0";
+if(testimonial){
+    window.onscroll=()=>{
+        if(window.scrollY >= testimonial.offsetTop - 600){
+            testimonialBox.style.right = "0";
+        }
     }
 }
 let date = new Date();
 dateNow.innerHTML=date.getFullYear();
 footerUlis.forEach((li)=>{
     li.addEventListener("click",(e)=>{
-        footerUlis.forEach((li)=>{
-            li.classList.remove("active");
-            e.currentTarget.classList.add("active");
-        })
+    removeActiveFooter();
+    console.log(e.currentTarget)
+    e.currentTarget.classList.add("active");
     })
 })
+function removeActiveFooter(){
+    footerUlis.forEach((li)=>{
+        li.classList.remove("active");
+    })
+}
